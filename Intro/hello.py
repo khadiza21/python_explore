c82c9b928ecbffc34ecc833874c6699720b351e8
khadiza21 <bibikhadiza474@gmail.com>
2023-07-28 22:06:39 +0600

    printing , comment and backslash character in python

--- Intro/hello.py
+++ Intro/hello.py
@@ -1,1 +1,14 @@
+#Print in python
 print("hello bk!")
+
+
+#comment
+# Welcome to learn python explore
+''' 
+THis is the journey to explore python. 
+Happy learing . 
+'''
+
+
+# backslash character
+print ("Name \t\t Hobbies \nKhadiza \t Reading")
