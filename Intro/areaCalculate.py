3e9b2eb56132ee47d688d74e8d188e11504e3cc1
khadiza21 <bibikhadiza474@gmail.com>
2023-07-28 22:57:44 +0600

    area calculation

---
+++ Intro/areaCalculate.py
@@ -0,0 +1,16 @@
+#Triangle area
+print("Triangle area")
+base=float(input("Enter Base = "))
+height = float(input("Enter Height = "))
+
+t_area = 0.5*base*height
+print("\nTriangle area is " ,t_area)
+
+
+#Circle area
+print("\n\nCircle area")
+radius=float(input("Enter radius = "))
+
+c_area = 3.1416*radius*radius
+print("\n Circle area is " ,c_area)
+
