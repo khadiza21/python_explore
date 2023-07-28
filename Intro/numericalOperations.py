7f01785fb9062feae4ec540e7e3067cb83384d7f
khadiza21 <bibikhadiza474@gmail.com>
2023-07-28 22:23:19 +0600

    numerical Operations

---
+++ Intro/numericalOperations.py
@@ -0,0 +1,20 @@
+print (11+4)
+print ( 11 -4)
+print (11 * 4 )
+print(11/ 4)
+print(11 %4)
+
+a=20
+b=6
+print ('ADDITON ',a+b)
+print ( 'Substruction ',a -b)
+print ('Multiplication ',a * b )
+print('Division ',a/ b)
+print('Modulus ',a %b)
+
+# floor(below purno number)
+print ('FLoor Point value ',a//b)
+# celi(upper purno number)
+# print('',)
+
+print ('Exponential value ',2**3) # exponential value
