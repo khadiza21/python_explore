e64d78495a0efcd89ab5ad1e7c853ad26ec40eb6
khadiza21 <bibikhadiza474@gmail.com>
2023-07-29 00:18:30 +0600

    math library function of python

---
+++ Intro/math_library_func.py
@@ -0,0 +1,30 @@
+from math import *
+
+# print(max('Maximum value ', (20,10))) error
+
+
+print('Maximum value',(max(20,10)))
+print('Minimum value ',(min(24,43)))
+print('Absolute value ',(abs(-4)))
+print('Power value ',(pow(2,4)))
+print('Square Root value ',(sqrt(25)))
+print('Round value ',(round(3.5)))
+
+# floor(below purno number)
+print ('Floor Point Value ',(floor(2.7)))
+# celi(upper purno number)
+print ('Ceiling Point Value ',(ceil(2.7)))
+
+
+
+'''
+Outputs : 
+Maximum value 20
+Minimum value  24
+Absolute value  4
+Power value  16.0
+Square Root value  5.0
+Round value  4
+Floor Point Value  2
+Ceiling Point Value  3
+'''
---
+++ Intro/tempCodeRunnerFile.py
@@ -0,0 +1,1 @@
+print(min('Minimum value ',(24,43)))
