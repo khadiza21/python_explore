4f65752c646dca529f1b815e157e655674173aac
khadiza21 <bibikhadiza474@gmail.com>
2023-07-28 22:45:34 +0600

    type casting  in python

---
+++ Intro/typeCasting.py
@@ -0,0 +1,6 @@
+num1=input("Enter First Number: ") # input will be taken as string value
+num2=float(input("Enter Second Number: "))
+sum=int(num1)+(num2) # so converted value as integer and float
+print ("The sum is ",sum)
+
+
