18ce63bee72b5b5356690b4b8b21a6e74c787849
khadiza21 <bibikhadiza474@gmail.com>
2023-07-29 19:47:04 +0600

    formatting string

---
+++ Intro/formattedString.py
@@ -0,0 +1,18 @@
+num =24.4
+name='Tanu'
+choise=True
+
+print (type(num))
+print(type(name))
+print(type(choise))
+
+num1=10
+num2=90
+#for formatting any value of var used second bracetes in the quotaion with f.
+print(f"The some of {num1} + {num2} = {num2+num1} ")
+
+
+print("This is Python 3.11 version.", end=" ") #here end="" will print in the same line
+print("Python is Interpreted, high level, object oriented language.")
+
+
