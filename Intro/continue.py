868ec68282413cb8292d5af6fdc2c730afff58f1
khadiza21 <bibikhadiza474@gmail.com>
2023-07-31 22:31:27 +0600

    loop, continue, increment operator in python

---
+++ Intro/continue.py
@@ -0,0 +1,10 @@
+i = 1
+while i <= 100:
+    i +=1
+    if i == 20:
+        continue
+    print(i)
+    if i ==30:
+        break
+
+print("HELLO")
---
+++ Intro/loop.py
@@ -0,0 +1,8 @@
+num = int(input("Enter the last number: "))
+sum = 0
+i =1 
+while i <= n:
+    sum = sum +1
+    i = i+1
+print(sum)
+
