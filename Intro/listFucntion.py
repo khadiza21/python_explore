7af008f1e1bc303d2b1ebf8899c5c5451068c3db
khadiza21 <bibikhadiza474@gmail.com>
2023-08-10 23:47:28 +0600

    list in python

---
+++ Intro/listFucntion.py
@@ -0,0 +1,9 @@
+subjects = ["C","C++", "java", "python", "Basic"]
+print(len(subjects))
+subjects.append("Toc")
+subjects.insert(3,"ox")
+print(subjects)
+subjects.remove("Basic")
+print(subjects)
+subjects.sort()
+print(subjects)
